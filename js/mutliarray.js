ff6c39d22eb77161c6a7fa7f61d95645d05b2741
dhanshree2022 <shinde.dhanshree143@gmail.com>
2023-10-04 08:50:20 +0530

    done

--- js/mutliarray.js
+++ js/mutliarray.js
@@ -1,13 +1,24 @@
 let arr = [
-    [0,'dhanshu',25],
-    [1,'soni', 26],
-    [2,'dolly',24],
-    [3,'neha',25]
+    [0, 'dhanshu', 25],
+    [1, 'soni', 26],
+    [2, 'dolly', 24],
+    [3, 'neha', 25]
 ];
 console.log(arr);
+let print = '<table border="1"> <th>ID</th><th>NAME</th><th>AGE</th>';
+for (let i = 0; i < arr.length; i++) {
+    print = print + '<tr>';
 
-const multiArray=()=>{
-    for(let i =0; i<arr.length;i++){
+    for (let j = 0; j < arr[i].length; j++) {
 
+        print = print + "<td>";
+        print = print + arr[i][j];
+        print = print + "</td>";
     }
+    print = print + '</tr>';
 }
+print = print + '</table>';
+
+document.getElementById("table").innerHTML = print;
+
+
